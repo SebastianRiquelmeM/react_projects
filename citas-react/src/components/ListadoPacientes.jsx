025fe41b42c143e7ddd45eac10d267d0e2eda5f5
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-01-17 04:53:25 -0300

    diseño terminado

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { Paciente } from "./Paciente";
 
 export const ListadoPacientes = () => {
 	return (
-		<div className="w-1/2 md:w-3/5">
-			<h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+		<div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
+			<h2 className="font-black text-3xl text-center">
+				Listado Pacientes
+			</h2>
 			<p className="text-xl mt-5 mb-10 text-center">
 				Administra tus {""}
-				<span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+				<span className="text-indigo-600 font-bold">
+					Pacientes y Citas
+				</span>
 			</p>
-
-			<div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
-				<p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
-					<span className="font-normal">Hook</span>
-				</p>
-			</div>
+			<Paciente/>
+			<Paciente/>
+			<Paciente/>
+			<Paciente/>
+			<Paciente/>	
 		</div>
 	);
 };
---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Paciente = () => {
+	return (
+		<div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+			<p className="font-bold mb-3 text-gray-700 uppercase">
+				Nombre: {""}
+				<span className="font-normal normal-case">Lulú</span>
+			</p>
+			<p className="font-bold mb-3 text-gray-700 uppercase">
+				Propietario: {""}
+				<span className="font-normal normal-case">Sebastián</span>
+			</p>
+			<p className="font-bold mb-3 text-gray-700 uppercase">
+				Email: {""}
+				<span className="font-normal normal-case">Seba@google.com</span>
+			</p>
+			<p className="font-bold mb-3 text-gray-700 uppercase">
+				Fecha de alta: {""}
+				<span className="font-normal normal-case">
+					10 Diciembre de 2022
+				</span>
+			</p>
+		</div> 
+	);
+};
