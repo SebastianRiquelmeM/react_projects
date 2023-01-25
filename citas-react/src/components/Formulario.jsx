4a727b6caaf7f0050a5eab7f2b69be24cbb28030
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-01-24 22:09:30 -0300

    Agregando hooks y submit

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export const Formulario = () => {
+	const [nombre, setNombre] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("Formulario enviado");
+	};
+
 	return (
 		<div className="md:w-1/2 lg:w-2/5">
 			<h2 className="font-black text-3xl text-center">
@@ -10,7 +18,10 @@ export const Formulario = () => {
 				Añade Pacientes y {""}
 				<span className="text-indigo-600 font-bold">Administralos</span>
 			</p>
-			<form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+			<form
+				onSubmit={handleSubmit}
+				className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+			>
 				<div className="mb-5">
 					<label
 						htmlFor="mascota"
@@ -23,6 +34,8 @@ export const Formulario = () => {
 						type="text"
 						placeholder="Nombre de la mascota"
 						className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+						value={nombre}
+						onChange={(e) => setNombre(e.target.value)}
 					></input>
 				</div>
 				<div className="mb-5">
