5924a74e6f5e23f0c3bdc4654add9af29b3f748a
Sebastian Riquelme <sebarsalbo@gmail.com>
2023-03-17 20:39:44 -0300

    Form funciona para definir presupuesto, falta validar el form para no tener letras

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const NuevoPresupuesto = () => {
+export const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
 	return (
 		<div className="contenedor-presupuesto contenedor sombra">
 			<form className="formulario">
@@ -10,10 +10,12 @@ export const NuevoPresupuesto = () => {
 						className="nuevo-presupuesto"
 						type="text"
 						placeholder="Añade tu presupuesto"
-                         />
+						value={presupuesto}
+						onChange={(e) => setPresupuesto(e.target.value)}
+					/>
 				</div>
 
-                    <input type='submit' value='Añadir'/>
+				<input type="submit" value="Añadir" />
 			</form>
 		</div>
 	);
